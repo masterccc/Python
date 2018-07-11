a2ad91cf369f2418ea60d4843b88c1b537e4a92c
masterccc <35172328+masterccc@users.noreply.github.com>
2018-07-11 23:26:28 +0200

    minimalize

--- Others/PyConverter.py
+++ Others/PyConverter.py
@@ -3,26 +3,25 @@
 
 import argparse
 
+def display(first_color, letter, data):
+	return green + data + default + " -> " + green + ("{0:"+letter+"}").format(int(data)) + default
+
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("Data", type=str, metavar="Data", help="Data to convert, must be an integer.")
 parser.add_argument("Key", type=str, metavar="Key", help="Integer convert to b = Binary, x = Hexa or o = Octal.")
 
 args = parser.parse_args()
-
 # Colors
 green = "\033[1:32m"
 warning ="\033[1:33m" 
 default = "\033[0m" 
 
+error_msg = warning + "[Error] Impossible to convert your datas with this key." + default
+
+tformat = args.Key.lower()
+
 try:
-    if args.Key.lower() == "x":
-        print green + args.Data + default + " -> " + green + "{0:x}".format(int(args.Data)) + default
-    elif args.Key .lower() == "b":
-        print green + args.Data + default + " -> " + green + "{0:b}".format(int(args.Data)) + default
-    elif args.Key.lower() == "o" :
-        print green + args.Data + default + " -> " + green + "{0:o}".format(int(args.Data)) + default
-    else:
-        print warning + "[Error] Impossible to convert your datas with this key." + default
+	print display(green, tformat, args.Data) if (tformat in ["x","b","o"]) else error_msg
 except ValueError as e:
     print warning + "[ValueError] The data must be integer." + default
 
